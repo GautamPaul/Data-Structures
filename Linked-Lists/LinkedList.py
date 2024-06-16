d3626141d27326cbdaf4426a6f9ef29e182f7baf
Gautam Paul <paulgautam6789@gmail.com>
2024-06-16 09:49:49 +0530

    Code for insert_data_at_position, type hints, changed message for add_node_after_value, corrected logic for insert_data_at_beginning

--- Linked-Lists/LinkedList.py
+++ Linked-Lists/LinkedList.py
@@ -36,21 +36,21 @@ class LinkedList():
 
     # method to add node in linked list
     # check if the length of the linked list is 0, if so add the node at beginning of the list, else add at the end of the list
-    def add_node(self, node):
+    def add_node(self, node: Node):
         if self.length == 0:
             self.add_node_at_beginning(node)
         else:
             self.add_node_at_last(node)
 
     # method to add node at the beginning of the linked list
-    def add_node_at_beginning(self, node):
+    def add_node_at_beginning(self, node: Node):
         new_node = node                 # create a temporary node
         new_node.next = self.head       # set next of temporary node to be current head of linked list
         self.head = new_node            # set head of the linked list to the temporary node
         self.length += 1                # increase length of the linked list by 1
 
     # method to add node at the last
-    def add_node_at_last(self, node):
+    def add_node_at_last(self, node: Node):
         current_node = self.head                # set head as the current node
         while current_node.next != None:        # iterate to find the last node of the linked list
             current_node = current_node.next
@@ -61,7 +61,7 @@ class LinkedList():
 
 
     # method to insert node after a certain with given data
-    def add_node_after_value(self, data, node):
+    def add_node_after_value(self, data, node: Node):
         new_node = node                 # create temporary node 
         current_node = self.head        # set head of linked list as the current node
 
@@ -88,7 +88,7 @@ class LinkedList():
 
         # if the position given is greater than current list length, or lesser than 0, then it is invalid position
         if position>self.length or position<0:
-            print(f"The given position does not exist in linked list. Current length: {self.length}")
+            print(f"The given position does not exist in linked list. Positions available: 0-{self.length}")
         elif position == 1:                             # if the target position is 1
             self.add_node_at_beginning(new_node)        # then the node has to be added at beginning
         else:
@@ -116,6 +116,7 @@ class LinkedList():
     # method to insert data at the beginning of the linked list
     def insert_data_at_beginning(self, data):
         new_node = Node(data)           # create a new node with data
+        new_node.next = self.head       # set next of new node to point current head
         self.head = new_node            # set head of the linked list to point to new node
         self.length += 1                # increase length of the linked list by 1
 
@@ -148,6 +149,32 @@ class LinkedList():
                 current_node = current_node.next        # move to next node of the linked list
         print(f"The data provided, {after_value}, is not present in the linked list.")
 
+
+    # method to insert data at given position
+    def insert_data_at_position(self, position, data):
+        count = 0                       # set count to track position
+        previous_node = self.head       # set previous node to point at head of linked list
+        current_node = self.head        # set current node to point at head of linked list
+
+        # check if position is valid
+        if position>self.length or position<0:
+            print(f"The given position does not exist in linked list. Positions available: 0-{self.length}")
+        elif position == 1:                         # if position is 1
+            self.insert_data_at_beginning(data)     # insert data at the beginning of the linked list
+        else:
+            # iterate through list till last node is reached or count goes past given position
+            while current_node.next != None or count<position:
+                count += 1                              # firstly, increase count position by 1, since it is initialized at 0
+                if count == position:                   # if the position is reached
+                    new_node = Node(data)               # create a new node with the given data
+                    new_node.next = current_node        # set next of the new node to point at the current node
+                    previous_node.next = new_node       # set next of the previous node to point at the new node
+                    self.length += 1                    # increase length of the linked list by 1
+                    return
+                else:
+                    previous_node = current_node        # if the position is reached
+                    current_node = current_node.next    # move to next node
+
     # method to print content of linked list
     def print_linked_list(self):
         linked_list = []
@@ -171,4 +198,7 @@ ll.print_linked_list()
 
 
 ll.add_node_at_position(5, Node(7))
+ll.print_linked_list()
+
+ll.insert_data_at_position(6,8)
 ll.print_linked_list()
