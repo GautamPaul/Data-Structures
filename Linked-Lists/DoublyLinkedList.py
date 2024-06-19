a1d0fa89625020fca06d2254bcac13755f8dba4a
Gautam Paul <paulgautam6789@gmail.com>
2024-06-19 22:35:51 +0530

    Code for find_data

--- Linked-Lists/DoublyLinkedList.py
+++ Linked-Lists/DoublyLinkedList.py
@@ -72,6 +72,19 @@ class DoublyLinkedList:
             current_head.previous = self.head               # set previous of the current head value to point to the head
             self.length += 1                                # increase the length of the doubly linked list by 1
 
+    # method to find if data is present in the doubly linked list
+    def find_data(self, data):
+        count = 0                               # set count as 0, to track the position in the traversal
+        current_node = self.head                # set current node to point to the head
+        while current_node != None:             # iterate through the doubly linked list
+            count += 1                          # increase the count by 1
+            if current_node.data == data:       # check if the current node has the target data
+                print(f"{data} is present in the doubly linked list at position: {count}")
+                return True                     # data found
+            current_node = current_node.next    # move to next node
+        print(f"{data} is not present in the doubly linked list.")
+        return False                            # data not found
+
     # method to print the doubly linked list
     def print_doubly_linked_list(self):
         if self.head == None:                               # if head is None, it means there are no elements
@@ -104,3 +117,5 @@ dll.insert_at_end(3)
 dll.insert_at_beginning(-1)
 dll.print_doubly_linked_list()
 dll.reverse_print_doubly_linked_list()
+print(dll.find_data(3))
+print(dll.find_data(-3))
