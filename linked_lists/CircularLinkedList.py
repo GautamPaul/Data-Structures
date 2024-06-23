b302c8d021d70352dcf15e3de09a5d2c311ce496
Gautam <paulgautam6789@gmail.com>
2024-06-23 11:31:45 +0530

    Code for delete_from_beginning

--- linked_lists/CircularLinkedList.py
+++ linked_lists/CircularLinkedList.py
@@ -62,6 +62,18 @@ class CircularLinkedList:
             self.head = new_node                    # set head to point to new node
         self.length += 1                            # increase length of the circular linked list by 1
 
+    # method to delete node from beginning
+    def delete_from_beginning(self):
+        if self.length == 1:                        # check if there is only one node
+            self.head = None                        # set head to point to None
+        else:
+            current_node = self.head                # set head as the current node
+            while current_node.next != self.head:   # iterate to get the last node
+                current_node = current_node.next    # move to next node
+            self.head = self.head.next              # set next of head to be new head
+            current_node.next = self.head           # set next of last node to point to new head
+        self.length -= 1                            # decrease length of the circular linked list by 1
+
 
     # method to print the nodes in the circular linked list
     def print_circular_linked_list(self):
@@ -85,6 +97,10 @@ def main():
     cll.print_circular_linked_list()
     print("inserting 1 at beginning")
     cll.add_node_at_beginning(1)
+    cll.print_circular_linked_list()
+    print("deleting node from beginning")
+    cll.delete_from_beginning()
+    cll.print_circular_linked_list()
     print("inserting 2 at beginning")
     cll.add_node_at_beginning(2)
     print("printing cll")
@@ -94,6 +110,10 @@ def main():
     cll.add_node_at_end(4)
     print("printing cll")
     cll.print_circular_linked_list()
+    print("deleting from beginning")
+    cll.delete_from_beginning() 
+    cll.print_circular_linked_list()
+    
 
 if __name__ == "__main__":
     main()
