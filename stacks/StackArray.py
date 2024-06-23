f419b20acb9a128fe96d502df136f3cd3f92daf0
Gautam <paulgautam6789@gmail.com>
2024-06-23 18:05:14 +0530

    code for pop, is_empty, is_full

--- stacks/StackArray.py
+++ stacks/StackArray.py
@@ -5,12 +5,27 @@ class Stack:
         self.size = size
         self.stack = []
 
+    # method to check if stack is empty
+    def is_empty(self):
+        return len(self.stack) == 0
+
+     # method to check if stack is full
+    def is_full(self):
+        return len(self.stack) == self.size
+
     # method to push element in the stack
     def push(self, data):
-        if len(self.stack) == self.size:                # check if the stack is full
+        if self.is_full():                              # check if the stack is full
             print("Stack overflow. Stack is full.")
         else:
             self.stack.append(data)                     # append the data in the stack
+
+    # method to pop element from the stack
+    def pop(self):
+        if self.is_empty():                             # check if the stack is empty
+            print("Stack underflow. Stack is empty.")
+        else:
+            return self.stack.pop()                     # pop the top element from stack
 
     # method to print data in stack
     def print_stack(self):
@@ -19,9 +34,12 @@ class Stack:
 
 def main():
     stack = Stack(5)
+    stack.pop()
     stack.push(5)
     stack.print_stack()
     stack.push(1)
+    stack.print_stack()
+    print(stack.pop())
     stack.print_stack()
 
 if __name__ == "__main__":
