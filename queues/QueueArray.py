31af3b1c9df1e995b4e6a7596cc190ca35bef61e
Gautam <paulgautam6789@gmail.com>
2024-06-24 23:01:19 +0530

    Code for is_empty, get_size, get_rear_element, get_front_element ;
    '

--- queues/QueueArray.py
+++ queues/QueueArray.py
@@ -5,4 +5,25 @@ class Queue:
         self.front = None
         self.rear = None
         self.size = 0
-        
+    
+    def is_empty(self):
+        return self.size == 0
+    
+    def get_size(self):
+        return self.size
+    
+    def get_rear_element(self):
+        if self.rear is None:
+            return "Queue is empty."
+        else:
+            return self.queue[self.rear]
+
+    def get_front_element(self):
+        if self.front is None:
+            return "Queue is empty."
+        else:
+            return self.queue[self.front]
+
+
+queue = Queue(5)
+print(queue.get_front_element())
