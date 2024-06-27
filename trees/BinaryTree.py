baabc028f47712cde3da7206061a9a7fc07f4c44
Gautam <paulgautam6789@gmail.com>
2024-06-27 22:40:42 +0530

    Comments

--- trees/BinaryTree.py
+++ trees/BinaryTree.py
@@ -1,3 +1,4 @@
+# class for Node of binary tree
 class Node:
     def __init__(self, data):
         self.data = data
@@ -8,29 +9,31 @@ class Node:
         return f"Data: {self.data}"
 
 
+# class for Binary Tree
 class BinaryTree:
     def __init__(self, data):
         self.root = Node(data)
         self.left = None
         self.right = None
-    
+
+    # method to insert node in left of binary tree
     def insert_left(self, data):
-        new_node = Node(data)
-        if self.left is None:
-            self.left = new_node
+        new_node = Node(data)           # create a new node with data
+        if self.left is None:           # check if there is any left of binary tree
+            self.left = new_node        # set left of binary tree to point to new node
         else:
-            new_node.left = self.left
-            self.left = new_node
-        self.root.left = self.left
+            new_node.left = self.left   # set left of new node to point to current left of binary tree
+            self.left = new_node        # make this new node as left of binary tree
+        self.root.left = self.left      # set left of root to point to left of binary tree
 
     def insert_right(self, data):
-        new_node = Node(data)
-        if self.right is None:
-            self.right = new_node
+        new_node = Node(data)           # create a new node with data
+        if self.right is None:          # check if there is any right of binary tree
+            self.right = new_node       # set right of binary tree to point to new node
         else:
-            new_node.right = self.right
-            self.right = new_node
-        self.root.right = self.right
+            new_node.right = self.right # set right of new node to point to current right of binary tree
+            self.right = new_node       # make this new node as right of binary tree
+        self.root.right = self.right    # set right of binary tree to point to new node
 
 bt = BinaryTree(0)
 bt.insert_left(1)
